c391440f57cd32f4f7ae38593a648bfd2d8b7ca5
truemcca <ltruemccarthy@mcad.edu>
2015-10-15 21:11:07 -0500

    Ex 8.1

---
+++ week8/ex_8.1/script.js
@@ -0,0 +1,13 @@
+function multiplyThenSubtract(a,b){
+	var result = a * b - b;
+	return result;
+}
+
+var x = multiplyThenSubtract(10,5);
+alert (x);
+
+var y = multiplyThenSubtract(100, 20);
+alert (y);
+
+var z = multiplyThenSubtract (200, 1);
+alert (z);
